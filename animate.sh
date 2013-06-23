a60994deca37932c1712becd043ee628ce353946
Mario Aeby <spam@eMeidi.com>
2013-06-23 22:59:07 +0200

    Globbing enabled

--- animate.sh
+++ animate.sh
@@ -6,6 +6,6 @@ FRAMESPERSEC=5
 EXT="JPG" # pay attention to lower- and uppercase
 DEST="flickerbook.mp4"
 
-$FFMPEG -f image2 -r $FRAMESPERSEC -pattern_type glob -i '*.$EXT' -c:v libx264 "$DEST"
+$FFMPEG -f image2 -r $FRAMESPERSEC -pattern_type glob -i '*.'$EXT -c:v libx264 "$DEST"
 
 exit 0
