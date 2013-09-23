05e4e2b7eaccffe179d29ebdb0f1c357ce32c61f
Mario Aeby <spam@eMeidi.com>
2013-09-24 00:33:46 +0200

    Lowercase extension. Increased speed. Checks.

--- animate.sh
+++ animate.sh
@@ -2,8 +2,8 @@
 
 FFMPEG=`which ffmpeg`
 
-FRAMESPERSEC=5
-EXT="JPG" # pay attention to lower- and uppercase
+FRAMESPERSEC=10 # the more frames the faster it looks
+EXT="jpg" # pay attention to lower- and uppercase
 DEST="flickerbook.mp4"
 
 $FFMPEG -f image2 -r $FRAMESPERSEC -pattern_type glob -i '*.'$EXT -c:v libx264 "$DEST"
--- link.sh
+++ link.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
-EXT="JPG"
+EXT="jpg" # pay attention to lower- and uppercase
+
+NUMSOURCEFILES=`find . -name "0*.$EXT" | wc -l`
+
+if [ $NUMSOURCEFILES -lt 1 ]
+then
+	echo "No files matching '0*.$EXT' found in current working dir. Check file upper- and lowercase extension. Abort."
+	exit 1
+else
+	echo "$NUMSOURCEFILES files matching '0*.$EXT' found in current working dir."
+	echo "Continuing."
+	echo ""
+fi
 
 COUNTER=1000
 ITERATIONS=10 # 11 pictures with 10 iterations with 5 frames per second result in a movie duration of 22 secs
 
 echo "Deleting all existing links starting with 1*.$EXT ..."
 rm -f 1*.$EXT # do not quote the string with the wildcard, otherwise bash won't glob it
+echo "Done."
+echo ""
 
 echo "Starting the loop ..."
 for i in $(seq 1 $ITERATIONS)
@@ -20,5 +34,8 @@ do
    done
 
 done
+
+echo "Done."
+echo ""
 
 exit 0
